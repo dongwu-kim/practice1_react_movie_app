775a2280e54dd273295af1edd86dcc997348f046
dongwu-kim <alduk7797@naver.com>
2020-12-29 09:23:30 +0900

    good morning

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 1. Components
 
    - HTML을 반환하는 함수
+   - JSX looks like HTML+JS
    - JSX 활용, index.js render내 `<APP />` 등으로 표현
    - index.js 내에서는 하나의 component를 render한다. -> App
    - 위를 위반하지 않기 위해 App.js 내에 component를 추가한다.
@@ -21,10 +22,28 @@
 
        - Child components
 
-         > 이러한 components에는 HTML과 동일하게 무제한으로 property를 할당할 수 있는데, data type에 상관없이 할당이 가능하다.
+         > 이러한 components에는 HTML과 동일하게 무제한으로 property를 할당할 수 있는데, **data type**에 상관없이 할당이 가능하다.
          > 또한, 할당된 property는 다시 외부 function으로 정의된 Father components의 arguments로 반환되어진다.
          > DOM의 형태를 가지고 있기 때문에, `console.log()`로 확인할 수 있다.
 
        - Father components
 
          > Child에게서 할당받은 components의 property를 활용해 우리는 다양한 작업을 진행할 수 있다. -> Js를 떠올려라!
+
+2. Dynamic Data
+
+   - Components를 Copy & Paste로 작성할 수는 없다.
+     > 물론 그러한 작성방식이 문제가 되는 것은 아니다. 그러나, 상대적으로 아름다운 코드를 작성하고 싶고, 유지보수를 쉽게 하기 위한 방법을 생각한다면 중복이 많은 Code는 지양해야 할 것이다. (매서드의 경우는 처리하는 이벤트에 따라 달라지지만 ㅠㅠ)
+   - 이에 우리는 새로운 방식으로 Code를 작성할 줄 알아야 한다.
+
+     1.1 Array, map() ? 그게 뭔데 ?
+
+     - 먼저 Array에 대해서는 자세한 설명을 생락한다. 자료구조에 대한 이해는 나중에 따로 Java를 다루며 설명할 수 있는 기회를 마련하고자 한다.
+
+       > 다만 쉽게 생각하자면, 일종의 자료**보관함**이라고 생각하면 된다. 우리가 사용하고자 하는 Data를 언제든 꺼내쓸 수 있고, 보관함 자체를 하나의 Data로 활용하고 싶다면 당연히 그럴 수 있다!
+
+     - 그렇다면 이러한 Array는 어디에 활용될까? 방금 써져 있듯, 우리는 `map()`이라는 기가 막힌 매서드를 호출하여 보관함의 역할을 보다 더 가시적으로 이해할 수 있게 될 것이다.
+
+       - `map()` 너 뭐니?
+
+       >
--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from "react";
 
-function Test() {
-  return <h2>what the</h2>;
+function Father(args) {
+  const props = args;
+  return <h2>what the {props}</h2>;
 }
+
+const propsArr = [
+  {
+    name: `니체`,
+    image: `https://www.google.com/imgres?imgurl=https%3A%2F%2Fgobooki.net%2Fwp-content%2Fuploads%2F2020%2F03%2F%25ED%2594%2584%25EB%25A6%25AC%25EB%2593%259C%25EB%25A6%25AC%25ED%259E%2588-%25EB%258B%2588%25EC%25B2%25B4.png&imgrefurl=https%3A%2F%2Fgobooki.net%2F%25ED%2594%2584%25EB%25A6%25AC%25EB%2593%259C%25EB%25A6%25AC%25ED%259E%2588-%25EB%258B%2588%25EC%25B2%25B4%25EC%259D%2598-%25EC%2583%259D%25EC%2595%25A0%25EC%2599%2580-%25EC%25B2%25A0%25ED%2595%2599-%25EC%25A0%2595%25EB%25A6%25AC%2F&tbnid=v5lVHqFbnHPWrM&vet=12ahUKEwj1ztDW9PHtAhUUx4sBHYoVACAQMygEegUIARDRAQ..i&docid=62vbmfUsh_lDJM&w=668&h=660&q=%EB%8B%88%EC%B2%B4&ved=2ahUKEwj1ztDW9PHtAhUUx4sBHYoVACAQMygEegUIARDRAQ`,
+  },
+  {
+    name: `스피노자`,
+    image: `https://www.google.com/imgres?imgurl=http%3A%2F%2Fsunday.joins.com%2F_data%2Fphoto%2F2012%2F07%2F22002349.jpg&imgrefurl=http%3A%2F%2Fblog.daum.net%2Fwindada11%2F8762206&tbnid=XTIKcyURcmcBNM&vet=12ahUKEwijxPzQ9PHtAhUiNaYKHRTbATQQMygDegUIARCqAQ..i&docid=8o7RFdfPQO61uM&w=645&h=630&q=%EC%8A%A4%ED%94%BC%EB%85%B8%EC%9E%90&ved=2ahUKEwijxPzQ9PHtAhUiNaYKHRTbATQQMygDegUIARCqAQ`,
+  },
+  {
+    name: `데카르트`,
+    image: `https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.ddanzi.com%2Ffiles%2Fattach%2Fimages%2F977701%2F899%2F362%2F501%2F60a7c61569605aa10df2e40747280480.jpg&imgrefurl=http%3A%2F%2Fwww.ddanzi.com%2FddanziNews%2F501362899&tbnid=BvvP9zFjuQhAZM&vet=12ahUKEwi7hszL9PHtAhW5y4sBHSJXA0kQMygEegUIARC4AQ..i&docid=tvaSYH5uiZ7SZM&w=800&h=452&q=%EB%8D%B0%EC%B9%B4%EB%A5%B4%ED%8A%B8&ved=2ahUKEwi7hszL9PHtAhW5y4sBHSJXA0kQMygEegUIARC4AQ`,
+  },
+  {
+    name: `칸트`,
+    image: `https://www.google.com/imgres?imgurl=https%3A%2F%2Ft1.daumcdn.net%2Fthumb%2FR720x0%2F%3Ffname%3Dhttp%3A%2F%2Ft1.daumcdn.net%2Fbrunch%2Fservice%2Fuser%2FTRE%2Fimage%2F_94vkV1VNDb_L5TLRPWxGKbm_20&imgrefurl=https%3A%2F%2Fbrunch.co.kr%2F%40%40TRE%2F1679&tbnid=XTWjvypXcpKnTM&vet=12ahUKEwjHoZHC9PHtAhVBNqYKHSw8BfUQMygAegUIARDSAQ..i&docid=RVMd94TFoKd9xM&w=720&h=628&q=%EC%B9%B8%ED%8A%B8&ved=2ahUKEwjHoZHC9PHtAhVBNqYKHSw8BfUQMygAegUIARDSAQ`,
+  },
+  {
+    name: `프로이트`,
+    image: `https://www.google.com/imgres?imgurl=http%3A%2F%2Fmblogthumb4.phinf.naver.net%2F20151006_71%2Fujbymca1_1444094105942jowX1_JPEG%2FSigmund-Freud-2.jpg%3Ftype%3Dw2&imgrefurl=http%3A%2F%2Fm.blog.naver.com%2Fujbymca1%2F220500837566&tbnid=r4k-FOtY8_6IOM&vet=12ahUKEwiLsJuX9PHtAhVFNqYKHcb6CnAQMygEegUIARDHAQ..i&docid=6HR7vZupoeaohM&w=740&h=1010&q=%ED%94%84%EB%A1%9C%EC%9D%B4%ED%8A%B8&ved=2ahUKEwiLsJuX9PHtAhVFNqYKHcb6CnAQMygEegUIARDHAQ`,
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <Test />
     </div>
   );
 }
