72e10860f2fcbf1a8dc147e64596e7219baf4227
dongwu-kim <alduk7797@naver.com>
2020-12-29 19:14:13 +0900

    저녁공부12.29

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
+  state = {
+    num: 0,
+  };
+  plus = () => {
+    this.setState({ num: 1 });
+  };
+  minus = () => {
+    this.setState({ num: -1 });
+  };
   render() {
-    return <h1>Hello, world</h1>;
+    return (
+      <div>
+        <h1>Hello, world</h1>
+        <h1>number_counter : {this.state.num}</h1>
+        <button onClick={this.plus}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
 }
 
