0675218ac65ec2a436abc2b3cc124cdb5990dc46
dongwu-kim <alduk7797@naver.com>
2021-01-12 13:59:22 +0900

    0112 afternoon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie.js"; // import function component
+import "./reset.css";
+import "./App.css";
 
 class App extends React.Component {
+  // state를 요구하면 class component로 작성합니다.
   state = {
     isLoading: true,
     movies: [], // movies 라는 prop을 empty-array로 정의한 후, value를 추가하는 형태입니다.
@@ -14,7 +17,6 @@ class App extends React.Component {
         data: { movies },
       },
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-
     /* 이러한 선언은 ES6의 문법으로, 처음 봤을 때에는 마치 `익명객체`와도 같이 보입니다.
      그러나 정의하는 변수는 애초에 객체 내에 포함되어 있습니다. (prettier는 이것을 명시해주죠.)
      movies 라는 객체 선언 이전에 우린 외부 YTS API를 fetch하고, 이는 비동기로 처리됩니다.
@@ -31,23 +33,29 @@ class App extends React.Component {
 
   componentDidMount() {
     this.getMovies();
-    // componentDidMount() 메소드는 render()의 최초실행 이후 가장 먼저 실행되는 메소드입니다.
-    // 그렇기 때문에 initializing 이후 동기 함수 처리가 가능하다고 생각하시면 됩니다.
-    // render() -> componentDidMount()
+    /* componentDidMount() 메소드는 render()의 최초실행 이후 가장 먼저 실행되는 메소드입니다.
+     그렇기 때문에 initializing 이후 동기 함수 처리가 가능하다고 생각하시면 됩니다.
+     render() -> componentDidMount() */
   }
 
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? `loading...`
-          : movies.map((movie) => (
-              /*  
-              map() 메소드를 활용해 우리는 Movie component props value를 순환식으로 작성합니다.
-              movies Array의 각 obj들을 하나씩 가져오는 것이죠.
-              사전에 API에서 sort_by=rating을 활용해 영화의 평점 순위에 따른 정렬을 완료했기 때문에
-              우린 map() 메소드만으로도 편하게 component props value를 삽입할 수 있습니다. */
+      <section className="container">
+        <div className="header">
+          <span className="header__title">My First React Movie App</span>
+        </div>
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              /* map() 메소드를 활용해 우리는 Movie component props value를 순환식으로 작성합니다.
+              movies Array의 각 obj들을 하나씩 가져오는 것이죠. 사전에 API에서 sort_by=rating을
+              활용해 영화의 평점 순위에 따른 정렬을 완료했기 때문에 우린 map() 메소드만으로도 편하게
+              component props value를 삽입할 수 있습니다. */
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -55,11 +63,14 @@ class App extends React.Component {
                 year={movie.year}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
-              // Movie의 props는 Movie.js에서 생성했습니다.
-              // Movie의 value의 경우, movies 라는 Array의 object를 기반으로 구성되었습니다.
+              /*Movie의 props는 Movie.js에서 생성했습니다. Movie의 value의 경우, movies 라는
+            Array의 object를 기반으로 구성되었습니다.*/
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, title, year, summary, poster }) {
-  return <h4>{title}</h4>;
+function Movie({ id, title, year, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__date">
+        <h4 className="movie__title">{title}</h4>
+        <h5 className="movie__year">{year}</h5>
+        <h5 className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </h5>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 } //function component
 
 Movie.propTypes = {
@@ -13,6 +30,7 @@ Movie.propTypes = {
   year: PropTypes.number.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string.isRequired),
 };
 
 export default Movie;
